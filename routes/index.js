e05202db48f7e08bc966be26fdaf130750211ffa
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-07-01 18:57:40 +0500

    Setup routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var userController = require('../controllers/userController');
+var messageController = require('../controllers/messageController');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', messageController.index);
+
+router.get('/sign-up', userController.signUpGet);
+router.post('/sign-up', userController.signUpPost);
+
+router.get('/login', userController.loginGet);
+router.post('/login', userController.loginPost);
+
+router.get('/log-out', userController.logOut);
+
+router.get('/membership', userController.becomeMemberGet);
+router.post('/membership', userController.becomeMemberPost);
 
 module.exports = router;
